309a1798fbd81908d0cc4b03a215beb65544bfef
OlgaYakoko <108624217+OlgaYakoko@users.noreply.github.com>
2022-07-24 21:56:45 +0100

    commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const userString = prompt();
+const userNumb = +prompt();
+const userSymbol = prompt();
+const userBoolean = confirm();
+
+let result = null;
+let resultWithSymbol = null;
+
+const padString = function (argString, argNumber, argSymbol, argBoolean) {
+
+    if(argString === null || argString === "" ){return console.log(`err, enter the string!`)};
+    if(!isNaN (argString)) {return console.log(`err, enter the number!`)};
+    if(argSymbol.length > 1) {return console.log(`err, enter only one symbol`)};
+
+    
+    if(argNumber < argString.length){
+        result = argString.substring(0, argNumber);  
+
+        if(argBoolean === true) {
+        resultWithSymbol = result + argSymbol
+        console.log(resultWithSymbol);
+        } else (resultWithSymbol = argSymbol + result)
+        console.log(resultWithSymbol);
+
+    } else (console.log(argString));
+}
+
+padString(userString, userNumb, userSymbol, userBoolean);
+
